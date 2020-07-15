dca7c6f8cc6c95be56abfb92ef79231fc8ecfe0b
clriley9 <claytonriley@unomaha.edu>
2020-07-15 11:16:46 -0500

    added question page

---
+++ QuizApp/View/Base.lproj/questionPageController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  QuizApp
+//
+//  Created by Clayton Riley on 7/13/20.
+//  Copyright © 2020 Crispin Corpuz. All rights reserved.
+//
+
+import UIKit
+
+class questionPageController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
